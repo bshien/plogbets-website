b10c6c35aef69c31455e432ef729f1d13a79c59a
Oliver Jiang <ojiang@berkeley.edu>
2021-01-13 12:53:24 -0800

    Added controllers for getting a single bet, participating in a bet, and deleting a bet.

--- plogbets-src/server/controllers/bets.js
+++ plogbets-src/server/controllers/bets.js
@@ -1,10 +1,21 @@
 import Bet from '../models/Bet.js';
 
-export const getBets = async (req, res) => {
+export const getBets = async (req, res) => { // user loads website
     try {
         const Bets = await Bet.find();
 
         res.status(200).json(Bets);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+export const getBet = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const bet = await Bet.findById(id);
+
+        res.status(200).json(bet);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
@@ -22,4 +33,28 @@ export const createBet = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+export const participateBet = async (req, res) => { // TODO
+    const { id } = req.params;
+    const { user, wager } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedBet = { user, wager, _id: id}; // TODO user and wager need to be added into the
+                                                // participants and wagers arrays of the MongoDB document.
+
+    await Bet.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedBet);
+} 
+export const deleteBet = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No bet with id: ${id}`);
+
+    await Bet.findByIdAndRemove(id);
+
+    res.json({ message: "Bet deleted successfully." });
+
+
 }
