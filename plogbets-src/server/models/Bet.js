0b7fd3da9cb57094b4310a26328da5024470f69d
Oliver Jiang <ojiang@berkeley.edu>
2021-01-13 12:21:53 -0800

    Defined bet schema.

--- plogbets-src/server/models/Bet.js
+++ plogbets-src/server/models/Bet.js
@@ -1,13 +1,24 @@
 import mongoose from 'mongoose';
 
 const betSchema = mongoose.Schema({
-    text: String,
-    plogcoinBet: Number,
+    privacy: boolean, // False if public
+    creator: String,
+    question: String,
     participants: [String],
+    wagers: [Number], // positive wagers for yes, negative wagers for no
     createdAt: {
         type: Date, 
         default: new Date()
     },
+    revealStartDate: {
+        type: Date, 
+        default: new Date() // TODO
+    },
+    revealEndDate:
+        type: Date, 
+        default: new Date() // TODO
+    },
+    answer: Number, // +1 if yes, -1 if no, 0 if not yet decided
 })
 
 const Bet = mongoose.model('Bet', betSchema);
