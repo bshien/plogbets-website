fdcef0b6b106d816b81d9719ccd56a3d8b102200
bshien <bshien5124@gmail.com>
2021-01-20 20:02:51 -0800

    fixed some minor errors

--- plogbets-src/server/models/Bet.js
+++ plogbets-src/server/models/Bet.js
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const betSchema = mongoose.Schema({
-    privacy: boolean, // False if public
-    creator: String,
-    question: String,
-    participants: [String],
-    wagers: [Number], // positive wagers for yes, negative wagers for no
-    createdAt: {
-        type: Date, 
-        default: new Date()
-    },
-    revealStartDate: {
-        type: Date, 
-        default: new Date() // TODO
-    },
-    revealEndDate:
-        type: Date, 
-        default: new Date() // TODO
-    },
-    answer: Number, // +1 if yes, -1 if no, 0 if not yet decided
-})
+  privacy: Boolean, // False if public
+  creator: String,
+  question: String,
+  participants: [String],
+  wagers: [Number], // positive wagers for yes, negative wagers for no
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  revealStartDate: {
+    type: Date,
+    default: new Date(), // TODO
+  },
+  revealEndDate: {
+    type: Date,
+    default: new Date(), // TODO
+  },
+  answer: Number, // +1 if yes, -1 if no, 0 if not yet decided
+});
 
-const Bet = mongoose.model('Bet', betSchema);
+const Bet = mongoose.model("Bet", betSchema);
 
 export default Bet;
